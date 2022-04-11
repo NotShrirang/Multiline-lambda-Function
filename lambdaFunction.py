f5ac774435b205a10c416f90d40f3d3948ac68dd
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-04-11 07:11:27 +0530

    Add files via upload

---
+++ lambdaFunction.py
@@ -0,0 +1,20 @@
+import os
+def lambdafunction(*args, caller):
+    for line in args:
+        with open('lambdaInternal.py', 'a') as f:
+            f.write(line)
+
+    with open('lambdaInternal.py', 'a') as f:
+        f.write("\n\ndef main():\n")
+        f.write(f"\treturn {caller}")
+
+    import lambdaInternal
+    ret = lambdaInternal.main()
+    os.remove('lambdaInternal.py')
+    return ret
+
+# syntax :
+#     print(func("def mul(a, b):",
+#             "\n\tz = a*b",
+#             "\n\treturn z",
+#             caller="mul(3, 6)"))    
