51f788d4a8e93fa1f7126a4384b0dc53cf5734c1
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-04-15 07:20:23 +0530

    Updated function

--- lambdaFunction.py
+++ lambdaFunction.py
@@ -1,17 +1,29 @@
 import os
 def lambdafunction(*args, caller):
-    for line in args:
-        with open('lambdaInternal.py', 'a') as f:
-            f.write(line)
+    '''
+    Function to write lambda function with multiple lines. 
 
+    Args : 
+        *args (str)  : Lines of code. One line per argument.
+        caller (str) : Function name to be executed.
+    
+    Returns : 
+        Returns whatever mentioned in *args.
+    
+    Syntax :
+        lambdafunction("arg1", "arg2", ..., caller='caller function')
+
+    '''
     with open('lambdaInternal.py', 'a') as f:
+        for line in args:
+            f.write(line)
         f.write("\n\ndef main():\n")
         f.write(f"\treturn {caller}")
-
-    import lambdaInternal
+        
+    import lambdaInternal    
     ret = lambdaInternal.main()
     os.remove('lambdaInternal.py')
     return ret
 
 # syntax :
-#     print(lambdafunction("def mul(a, b):", "\n\tz = a*b", "\n\treturn z", caller="mul(3, 6)"))    # prints : 18
+# print(lambdafunction("def mul(a, b):", "\n\tz = a*b", "\n\treturn z", caller="mul(3, 6)"))    # prints : 18
